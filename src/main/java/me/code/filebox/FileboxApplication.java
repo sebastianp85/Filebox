4c1dc9e44ec084101060e26d4072a6e26a6862f5
sebastianp85 <109860660+sebastianp85@users.noreply.github.com>
2023-11-08 15:59:08 +0100

    first commit

---
+++ src/main/java/me/code/filebox/FileboxApplication.java
@@ -0,0 +1,13 @@
+package me.code.filebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/code/filebox/FileboxApplicationTests.java
@@ -0,0 +1,13 @@
+package me.code.filebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
