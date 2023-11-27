d85cabf19119f83912d6c2a9486f13a210ecb190
sebastianp85 <109860660+sebastianp85@users.noreply.github.com>
2023-11-27 13:49:19 +0100

    Added constants for the claims in jwttokenprovider to reduce the risk of typos and also added comments

--- src/main/java/me/code/filebox/security/JwtTokenProvider.java
+++ src/main/java/me/code/filebox/security/JwtTokenProvider.java
@@ -2,6 +2,7 @@ package me.code.filebox.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.NoArgsConstructor;
@@ -10,46 +11,70 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
+/**
+ * Component responsible for handling JWT token generation, validation, and extraction.
+ */
 @Component
 @NoArgsConstructor
 public class JwtTokenProvider {
 
-    private final String secret = "keyboardcat-dsahb5332fdf521123@∞©$@|£dsad123213678";
-    private final Key key = Keys.hmacShaKeyFor(secret.getBytes());
+    private static final String SECRET = "keyboardcat-dsahb5332fdf521123@∞©$@|£dsad123213678";
+    private static final Key KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
 
+    private static final String CLAIM_ID = "id";
+    private static final String CLAIM_USERNAME = "username";
+
+    /**
+     * Generates a JWT token for the given user.
+     *
+     * @param user The user for whom the token is generated.
+     * @return The generated JWT token.
+     */
     public String generateToken(User user) {
         int id = user.getId();
         String username = user.getUsername();
 
         return Jwts.builder()
                 .setSubject(Integer.toString(id))
-                .claim("id", id)
-                .claim("username", username)
-                .signWith(key)
+                .claim(CLAIM_ID, id)
+                .claim(CLAIM_USERNAME, username)
+                .signWith(KEY)
                 .compact();
     }
 
+    /**
+     * Validates the provided JWT token.
+     *
+     * @param token The JWT token to validate.
+     * @return True if the token is valid, false otherwise.
+     */
     public boolean validate(String token) {
         try {
             Jwts.parserBuilder()
-                    .setSigningKey(key)
+                    .setSigningKey(KEY)
                     .build()
                     .parseClaimsJws(token);
             return true;
-        } catch (Exception e) {
+        } catch (JwtException e) {
             return false;
         }
     }
 
+    /**
+     * Extracts the username from the provided JWT token.
+     *
+     * @param token The JWT token from which to extract the username.
+     * @return The username extracted from the token, or null if extraction fails.
+     */
     public String getUsernameFromToken(String token) {
         try {
             Jws<Claims> claimsJws = Jwts.parserBuilder()
-                    .setSigningKey(key)
+                    .setSigningKey(KEY)
                     .build()
                     .parseClaimsJws(token);
 
-            return claimsJws.getBody().get("username", String.class);
-        } catch (Exception e) {
+            return claimsJws.getBody().get(CLAIM_USERNAME, String.class);
+        } catch (JwtException e) {
             return null;
         }
     }
