47c6736cfb4fadb6672dc2dce20fc09a480d7587
sebastianp85 <109860660+sebastianp85@users.noreply.github.com>
2023-11-29 10:12:08 +0100

    Added dotenv dependency, changed the connection string to db to variable names and saved the names in a .env file instead

--- src/main/java/me/code/filebox/FileboxApplication.java
+++ src/main/java/me/code/filebox/FileboxApplication.java
@@ -1,5 +1,6 @@
 package me.code.filebox;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FileboxApplication {
 
     public static void main(String[] args) {
+        // Läs in .env-filen
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        // Sätt miljövariabler
+        System.setProperty("DB_CONNECTION", dotenv.get("DB_CONNECTION"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
         SpringApplication.run(FileboxApplication.class, args);
     }
 
